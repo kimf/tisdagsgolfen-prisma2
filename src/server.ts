7e30d597ac54650ee3622e23d289dcec1683d085
Kim Fransman <kim.fransman@gmail.com>
2020-03-15 14:37:38 +0100

    Prisma2 instead of keystonejs

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import * as path from 'path'
+import { GraphQLServer } from 'graphql-yoga'
+import { makeSchema } from 'nexus'
+import { nexusPrismaPlugin } from 'nexus-prisma'
+
+import * as types from './types'
+
+new GraphQLServer({
+  schema: makeSchema({
+    types,
+    plugins: [nexusPrismaPlugin()],
+    outputs: {
+      typegen: path.join(
+        __dirname,
+        '../node_modules/@types/nexus-typegen/index.d.ts'
+      )
+    }
+  })
+}).start(() =>
+  console.log(
+    `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-sdl-first#3-using-the-graphql-api`
+  )
+)
---
+++ src/types.ts
@@ -0,0 +1,40 @@
+import { queryType, mutationType, objectType } from 'nexus'
+
+export const Query = queryType({
+  definition(t) {
+    t.crud.seasons()
+    t.crud.events()
+    t.crud.players()
+  }
+})
+
+export const Mutation = mutationType({
+  definition(t) {
+    t.crud.createOneSeason()
+    t.crud.createOneEvent()
+  }
+})
+
+export const Season = objectType({
+  name: 'Season',
+  definition(t) {
+    t.typeName
+    t.model.id()
+    t.model.status()
+  }
+})
+
+export const Event = objectType({
+  name: 'Event',
+  definition(t) {
+    t.model.id()
+    t.model.status()
+  }
+})
+
+export const Player = objectType({
+  name: 'Player',
+  definition(t) {
+    t.model.id()
+  }
+})
