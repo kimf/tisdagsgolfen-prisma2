1d1f9772344124900fb5e93b82c281d98c92a622
Kim Fransman <kim.fransman@gmail.com>
2020-03-15 15:21:41 +0100

    Small changes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import * as path from 'path'
-import { GraphQLServer } from 'graphql-yoga'
-import { makeSchema } from 'nexus'
-import { nexusPrismaPlugin } from 'nexus-prisma'
+import * as path from 'path';
+import { GraphQLServer } from 'graphql-yoga';
+import { makeSchema } from 'nexus';
+import { nexusPrismaPlugin } from 'nexus-prisma';
 
-import * as types from './types'
+import * as types from './types';
 
 new GraphQLServer({
   schema: makeSchema({
@@ -12,12 +12,12 @@ new GraphQLServer({
     outputs: {
       typegen: path.join(
         __dirname,
-        '../node_modules/@types/nexus-typegen/index.d.ts'
-      )
-    }
-  })
+        '../node_modules/@types/nexus-typegen/index.d.ts',
+      ),
+    },
+  }),
 }).start(() =>
   console.log(
-    `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-sdl-first#3-using-the-graphql-api`
-  )
-)
+    `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-sdl-first#3-using-the-graphql-api`,
+  ),
+);
--- src/types.ts
+++ src/types.ts
@@ -1,40 +1,40 @@
-import { queryType, mutationType, objectType } from 'nexus'
+import { queryType, mutationType, objectType } from 'nexus';
 
 export const Query = queryType({
   definition(t) {
-    t.crud.seasons()
-    t.crud.events()
-    t.crud.players()
-  }
-})
+    t.crud.seasons();
+    t.crud.events();
+    t.crud.players();
+  },
+});
 
 export const Mutation = mutationType({
   definition(t) {
-    t.crud.createOneSeason()
-    t.crud.createOneEvent()
-  }
-})
+    t.crud.createOneSeason();
+    t.crud.createOneEvent();
+  },
+});
 
 export const Season = objectType({
   name: 'Season',
   definition(t) {
-    t.typeName
-    t.model.id()
-    t.model.status()
-  }
-})
+    t.typeName;
+    t.model.id();
+    t.model.status();
+  },
+});
 
 export const Event = objectType({
   name: 'Event',
   definition(t) {
-    t.model.id()
-    t.model.status()
-  }
-})
+    t.model.id();
+    t.model.status();
+  },
+});
 
 export const Player = objectType({
   name: 'Player',
   definition(t) {
-    t.model.id()
-  }
-})
+    t.model.id();
+  },
+});
